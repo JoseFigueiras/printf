b1f354f3f884cb9f1a8eef4323349785dc8b3a6e
Josefig <josefiggaming@gmail.com>
2021-03-19 18:46:35 +0000

    use valist in printf_percent

--- utilities/printf_percent.c
+++ utilities/printf_percent.c
@@ -4,6 +4,7 @@ int	printf_percent(va_list valist, t_flags flags)
 {
 	char	*output;
 
+	(void)valist;
 	output = ft_strdup('%');
 	if (flags.flag == '\0')
 		output = pad_left(output, flags.width, ' ');
